e8bf882f4cd88a6b7da680ad35b3c71be0e43e4c
15446-rus75 <vladabramov06@yandex.ru>
2026-03-05 21:24:01 +0300

    initial commit for repo

---
+++ taskplanner.cpp
@@ -0,0 +1,14 @@
+#include "taskplanner.h"
+#include "./ui_taskplanner.h"
+
+TaskPlanner::TaskPlanner(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::TaskPlanner)
+{
+    ui->setupUi(this);
+}
+
+TaskPlanner::~TaskPlanner()
+{
+    delete ui;
+}
---
+++ taskplanner.h
@@ -0,0 +1,23 @@
+#ifndef TASKPLANNER_H
+#define TASKPLANNER_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class TaskPlanner;
+}
+QT_END_NAMESPACE
+
+class TaskPlanner : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    TaskPlanner(QWidget *parent = nullptr);
+    ~TaskPlanner();
+
+private:
+    Ui::TaskPlanner *ui;
+};
+#endif // TASKPLANNER_H
