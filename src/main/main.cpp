0df043b68bab96f7ae141ba619d358ddc3b6fd6d
Cuathimbong <egvika20051@gmail.com>
2026-03-20 23:20:17 +0300

    CG 11 fixes and removing some insignificant changes from before

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    TaskPlanner w;
-    w.show();
-    return QCoreApplication::exec();
+  QApplication a(argc, argv);
+  TaskPlanner w;
+  w.show();
+  return QCoreApplication::exec();
 }
