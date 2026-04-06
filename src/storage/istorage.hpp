dfb1195c08a5b1a7ba13b67d4aeb8d0c0a4b84cc
15446-rus75 <vladabramov06@yandex.ru>
2026-04-06 15:06:42 +0300

    update IStorage

--- src/storage/istorage.hpp
+++ src/storage/istorage.hpp
@@ -5,6 +5,14 @@
 
 namespace storage
 {
+  enum class Priority
+  {
+    All,
+    Low,
+    Medium,
+    Hard
+  };
+
   class IStorage
   {
     virtual ~IStorage();
@@ -13,7 +21,11 @@ namespace storage
     virtual void updateTask(const Task &task) = 0;
     virtual QList< Task > getAllTasks() const = 0;
     virtual QList< Task > getTasksForDate(const QDate &date) const = 0;
-    virtual QList< Task > getSortedTasks() const = 0;
+    virtual QList< Task > getTasksForToday() const = 0;
+    virtual QList< Task > getOverdueTasks() const = 0;
+    virtual QList< Task > getTasksFiltered(const Qstring &searchText, bool today, bool overdue, Priority priority) const = 0;
+    virtual QList< Task > getSortedTasks(const QList< Task > &tasks, const QString &criterion) const = 0;
+    //criterion = date / priority / completed
     virtual void saveToFile(const QString &path) = 0;
     virtual void loadFromFile(const QString &path) = 0;
   };
