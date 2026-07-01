d4eab29a54fc59ed543ae4c7a68ecffe1b722e8f
15446-rus75 <vladabramov06@yandex.ru>
2026-07-01 15:45:53 +0300

    update controller test

--- test/controller/mockstorage.hpp
+++ test/controller/mockstorage.hpp
@@ -4,147 +4,220 @@
 #include "istorage.hpp"
 #include <QList>
 #include <QString>
-#include <QVector>
 
 namespace test
 {
-  class MockStorage: public storage::IStorage
+class MockStorage : public storage::IStorage
+{
+public:
+  MockStorage() = default;
+  ~MockStorage() override = default;
+
+  void addTask(const storage::Task &task) override
   {
-  public:
-    MockStorage() = default;
-    ~MockStorage() override = default;
+    addTaskCallCount++;
+    lastAddedTask = task;
+    m_tasks.append(task);
+  }
 
-    void addTask(const storage::Task &task) override
+  void removeTask(int id) override
+  {
+    removeTaskCallCount++;
+    lastRemovedId = id;
+    for (int i = 0; i < m_tasks.size(); ++i)
     {
-      addTaskCallCount++;
-      lastAddedTask = task;
-      m_tasks.append(task);
-    }
-
-    void removeTask(int id) override
-    {
-      removeTaskCallCount++;
-      lastRemovedId = id;
-      for (int i = 0; i < m_tasks.size(); ++i)
+      if (m_tasks[i].id == id)
       {
-        if (m_tasks[i].id == id)
-        {
-          m_tasks.removeAt(i);
-          return;
-        }
+        m_tasks.removeAt(i);
+        return;
       }
     }
+  }
 
-    void updateTask(const storage::Task &task) override
+  void updateTask(const storage::Task &task) override
+  {
+    updateTaskCallCount++;
+    lastUpdatedTask = task;
+    for (auto &existing : m_tasks)
     {
-      updateTaskCallCount++;
-      lastUpdatedTask = task;
-      for (auto &existing: m_tasks)
+      if (existing.id == task.id)
       {
-        if (existing.id == task.id)
-        {
-          existing = task;
-          return;
-        }
+        existing = task;
+        return;
       }
     }
+  }
 
-    QList< storage::Task > getAllTasks() const override
-    {
-      getAllTasksCallCount++;
-      return m_tasks;
-    }
+  QList< storage::Task > getAllTasks() const override
+  {
+    getAllTasksCallCount++;
+    return m_tasks;
+  }
 
-    QList< storage::Task > getTasksForDate(const QDate &date) const override
+  QList< storage::Task > getTasksForDate(const QDate &date) const override
+  {
+    getTasksForDateCallCount++;
+    lastQueriedDate = date;
+    QList< storage::Task > result;
+    for (const auto &task : m_tasks)
     {
-      getTasksForDateCallCount++;
-      lastQueriedDate = date;
-      QList< storage::Task > result;
-      for (const auto &task: m_tasks)
+      if (task.deadline.date() == date)
       {
-        if (task.deadline.date() == date)
-        {
-          result.append(task);
-        }
+        result.append(task);
       }
-      return result;
     }
+    return result;
+  }
 
-    QList< storage::Task > getTasksForToday() const override
+  QList< storage::Task > getTasksForToday() const override
+  {
+    getTasksForTodayCallCount++;
+    return tasksForTodayToReturn;
+  }
+
+  QList< storage::Task > getOverdueTasks() const override
+  {
+    getOverdueTasksCallCount++;
+    return overdueTasksToReturn;
+  }
+
+  QList< storage::Task > getTasksFiltered(const QString &searchText, bool today,
+                                        bool overdue, storage::Priority priority) const override
+  {
+    getTasksFilteredCallCount++;
+    lastSearchText = searchText;
+    lastTodayFlag = today;
+    lastOverdueFlag = overdue;
+    lastPriorityArg = priority;
+    return filteredTasksToReturn;
+  }
+
+  QList< storage::Task > getSortedTasks(const QList< storage::Task > &tasks,
+                                      storage::Criterion criterion) const override
+  {
+    getSortedTasksCallCount++;
+    lastSortedInput = tasks;
+    lastCriterion = criterion;
+    return tasks;
+  }
+
+  void saveToFile() noexcept override {}
+  void loadFromFile() noexcept override {}
+  void saveGamificationData() noexcept override {}
+  void loadGamificationData() noexcept override {}
+
+  storage::UserProgress getUserProgress() const override { return m_progress; }
+  void updateUserProgress(const storage::UserProgress &progress) override { m_progress = progress; }
+  void addXP(int amount, const QString &reason) override
+  {
+    Q_UNUSED(reason);
+    m_progress.currentXP += amount;
+  }
+  void updateStreak(const QDate &currentDate) override { Q_UNUSED(currentDate); }
+  int getCurrentLevel() const override { return m_progress.currentLevel; }
+  int getTotalXP() const override { return m_progress.currentXP; }
+  int getStreakDays() const override { return m_progress.streakDays; }
+  QList< storage::Achievement > getAllAchievements() const override { return allAchievementsToReturn; }
+  void unlockAchievement(const QString &achievementId) override
+  {
+    unlockAchievementCallCount++;
+    lastUnlockedAchievementId = achievementId;
+  }
+  bool isAchievementUnlocked(const QString &achievementId) const override
+  {
+    return m_progress.unlockedAchievementIds.contains(achievementId);
+  }
+  storage::Achievement getAchievementById(const QString &id) const override
+  {
+    for (const auto &a : allAchievementsToReturn)
     {
-      getTasksForTodayCallCount++;
-      return tasksForTodayToReturn;
+      if (a.id == id) return a;
     }
-
-    QList< storage::Task > getOverdueTasks() const override
+    return {};
+  }
+  QList< QString > getUnlockedLocations() const override { return m_progress.unlockedLocations; }
+  void unlockLocation(const QString &locationId) override { Q_UNUSED(locationId); }
+  bool isLocationUnlocked(const QString &locationId) const override
+  {
+    Q_UNUSED(locationId);
+    return false;
+  }
+  int getCompletedTasksCount() const override
+  {
+    int count = 0;
+    for (const auto &task : m_tasks)
     {
-      getOverdueTasksCallCount++;
-      return overdueTasksToReturn;
+      if (task.completed) ++count;
     }
-
-    QList< storage::Task > getTasksFiltered(const QString &searchText, bool today,
-      bool overdue, storage::Priority priority) const override
+    return count;
+  }
+  int getOnTimeCompletedCount() const override { return onTimeCompletedCount; }
+  int getCompletedCountByPriority(storage::Priority priority) const override
+  {
+    int count = 0;
+    for (const auto &task : m_tasks)
     {
-      getTasksFilteredCallCount++;
-      lastSearchText = searchText;
-      lastTodayFlag = today;
-      lastOverdueFlag = overdue;
-      lastPriorityArg = priority;
-      return filteredTasksToReturn;
+      if (task.completed && task.priority == priority) ++count;
     }
+    return count;
+  }
+  int getDeletedTasksCount() const override { return deletedTasksCount; }
+  int getPerfectDaysCount() const override { return perfectDaysCount; }
+  int getTotalLocationsCount() const override { return totalLocationsCount; }
+  int getMaxTasksCompletedInOneDay() const override { return maxTasksCompletedInOneDay; }
+  int getMaxHardTasksCompletedInOneDay() const override { return maxHardTasksCompletedInOneDay; }
 
-    QList< storage::Task > getSortedTasks(const QList< storage::Task > &tasks,
-      storage::Criterion criterion) const override
-    {
-      getSortedTasksCallCount++;
-      lastSortedInput = tasks;
-      lastCriterion = criterion;
-      return tasks;
-    }
+  // Вспомогательные методы для настройки мока
+  void setTasks(const QList< storage::Task > &tasks) { m_tasks = tasks; }
+  void setProgress(const storage::UserProgress &progress) { m_progress = progress; }
+  void setAllAchievements(const QList< storage::Achievement > &achievements)
+  {
+    allAchievementsToReturn = achievements;
+  }
 
-    void saveToFile(const QString &path) override
-    {
-      Q_UNUSED(path);
-    }
+  // Счётчики вызовов (camelCase для совместимости с тестами)
+  mutable int addTaskCallCount = 0;
+  mutable int removeTaskCallCount = 0;
+  mutable int updateTaskCallCount = 0;
+  mutable int getAllTasksCallCount = 0;
+  mutable int getTasksForDateCallCount = 0;
+  mutable int getTasksForTodayCallCount = 0;
+  mutable int getOverdueTasksCallCount = 0;
+  mutable int getTasksFilteredCallCount = 0;
+  mutable int getSortedTasksCallCount = 0;
+  mutable int unlockAchievementCallCount = 0;
 
-    void loadFromFile(const QString &path) override
-    {
-      Q_UNUSED(path);
-    }
+  // Сохранённые аргументы
+  storage::Task lastAddedTask;
+  storage::Task lastUpdatedTask;
+  int lastRemovedId = -1;
+  mutable QDate lastQueriedDate;
+  mutable QString lastSearchText;
+  mutable bool lastTodayFlag = false;
+  mutable bool lastOverdueFlag = false;
+  mutable storage::Priority lastPriorityArg = storage::Priority::All;
+  mutable QList< storage::Task > lastSortedInput;
+  mutable storage::Criterion lastCriterion = storage::Criterion::Date;
+  mutable QString lastUnlockedAchievementId;
 
-    void setTasks(const QList< storage::Task > &tasks)
-    {
-      m_tasks = tasks;
-    }
+  // Настраиваемые возвращаемые значения
+  QList< storage::Task > tasksForTodayToReturn;
+  QList< storage::Task > overdueTasksToReturn;
+  QList< storage::Task > filteredTasksToReturn;
+  QList< storage::Achievement > allAchievementsToReturn;
 
-    mutable int addTaskCallCount = 0;
-    mutable int removeTaskCallCount = 0;
-    mutable int updateTaskCallCount = 0;
-    mutable int getAllTasksCallCount = 0;
-    mutable int getTasksForDateCallCount = 0;
-    mutable int getTasksForTodayCallCount = 0;
-    mutable int getOverdueTasksCallCount = 0;
-    mutable int getTasksFilteredCallCount = 0;
-    mutable int getSortedTasksCallCount = 0;
+  // Счётчики для геймификации
+  int onTimeCompletedCount = 0;
+  int deletedTasksCount = 0;
+  int perfectDaysCount = 0;
+  int totalLocationsCount = 0;
+  int maxTasksCompletedInOneDay = 0;
+  int maxHardTasksCompletedInOneDay = 0;
 
-    storage::Task lastAddedTask;
-    storage::Task lastUpdatedTask;
-    int lastRemovedId = -1;
-    mutable QDate lastQueriedDate;
-    mutable QString lastSearchText;
-    mutable bool lastTodayFlag = false;
-    mutable bool lastOverdueFlag = false;
-    mutable storage::Priority lastPriorityArg = storage::Priority::All;
-    mutable QList< storage::Task > lastSortedInput;
-    mutable storage::Criterion lastCriterion = storage::Criterion::Date;
-
-    QList< storage::Task > tasksForTodayToReturn;
-    QList< storage::Task > overdueTasksToReturn;
-    QList< storage::Task > filteredTasksToReturn;
-
-  private:
-    QList< storage::Task > m_tasks;
-  };
+private:
+  QList< storage::Task > m_tasks;
+  storage::UserProgress m_progress{};
+};
 }
 
 #endif
--- test/controller/mockview.hpp
+++ test/controller/mockview.hpp
@@ -2,146 +2,179 @@
 #define MOCKVIEW_HPP
 
 #include "iview.hpp"
-#include "achievement.hpp"
+#include "../../src/utils/achievements.hpp"
 
 namespace test
 {
-  class MockView : public view::IView
+class MockView : public view::IView
+{
+public:
+  MockView() = default;
+  ~MockView() override = default;
+
+  void showTaskList(const QList< storage::Task > &tasks) override
   {
-  public:
-    MockView() = default;
-    ~MockView() override = default;
+    showTaskListCallCount++;
+    lastShownTasks = tasks;
+  }
 
-    void showTaskList(const QList<storage::Task> &tasks) override
-    {
-      showTaskListCallCount++;
-      lastShownTasks = tasks;
-    }
+  void showTasksForDate(const QDate &date, const QList< storage::Task > &tasks) override
+  {
+    showTasksForDateCallCount++;
+    lastDateArg = date;
+    lastShownTasks = tasks;
+  }
 
-    void showTasksForDate(const QDate &date, const QList<storage::Task> &tasks) override
-    {
-      showTasksForDateCallCount++;
-      lastDateArg = date;
-      lastShownTasks = tasks;
-    }
+  void setTaskListTitle(const QString &title) override
+  {
+    setTaskListTitleCallCount++;
+    lastTitle = title;
+  }
 
-    void setTaskListTitle(const QString &title) override
-    {
-      setTaskListTitleCallCount++;
-      lastTitle = title;
-    }
+  void showTaskCreationForm() override
+  {
+    showTaskCreationFormCallCount++;
+    lastFormHadTask = false;
+  }
 
-    void showTaskCreationForm() override
-    {
-      showTaskCreationFormCallCount++;
-      lastFormHadTask = false;
-    }
+  void showTaskCreationForm(const storage::Task &task) override
+  {
+    showTaskCreationFormCallCount++;
+    lastFormHadTask = true;
+    lastFormTask = task;
+  }
 
-    void showTaskCreationForm(const storage::Task &task) override
-    {
-      showTaskCreationFormCallCount++;
-      lastFormHadTask = true;
-      lastFormTask = task;
-    }
+  void closeTaskCreationForm() override
+  {
+    closeTaskCreationFormCallCount++;
+  }
 
-    void closeTaskCreationForm() override
-    {
-      closeTaskCreationFormCallCount++;
-    }
+  void showErrorMessage(const QString &message) override
+  {
+    showErrorMessageCallCount++;
+    lastErrorMessage = message;
+  }
 
-    void showErrorMessage(const QString &message) override
-    {
-      showErrorMessageCallCount++;
-      lastErrorMessage = message;
-    }
+  void showInfoMessage(const QString &message) override
+  {
+    showInfoMessageCallCount++;
+    lastInfoMessage = message;
+  }
 
-    void showInfoMessage(const QString &message) override
-    {
-      showInfoMessageCallCount++;
-      lastInfoMessage = message;
-    }
+  void updateStats(int total, int completed, int today) override
+  {
+    updateStatsCallCount++;
+    lastStatsTotal = total;
+    lastStatsCompleted = completed;
+    lastStatsToday = today;
+  }
 
-    void updateStats(int total, int completed, int today) override
-    {
-      updateStatsCallCount++;
-      lastStatsTotal = total;
-      lastStatsCompleted = completed;
-      lastStatsToday = today;
-    }
+  void showUserLevel(int level, int currentXP, int xpToNext) override
+  {
+    Q_UNUSED(level);
+    Q_UNUSED(currentXP);
+    Q_UNUSED(xpToNext);
+    showUserLevelCallCount++;
+  }
 
-    void showUserLevel(int level, int currentXP, int xpToNext) override
-    {
-      Q_UNUSED(level);
-      Q_UNUSED(currentXP);
-      Q_UNUSED(xpToNext);
-    }
+  void showStreak(int days) override
+  {
+    Q_UNUSED(days);
+    showStreakCallCount++;
+  }
 
-    void showStreak(int days) override
-    {
-      Q_UNUSED(days);
-    }
+  void showUserTitle(const QString &title) override
+  {
+    Q_UNUSED(title);
+    showUserTitleCallCount++;
+  }
 
-    void showUserTitle(const QString &title) override
-    {
-      Q_UNUSED(title);
-    }
+  void showXPNotification(int amount, const QString &reason) override
+  {
+    Q_UNUSED(amount);
+    Q_UNUSED(reason);
+    showXPNotificationCallCount++;
+  }
 
-    void showXPNotification(int amount, const QString &reason) override
-    {
-      Q_UNUSED(amount);
-      Q_UNUSED(reason);
-    }
+  void showAchievementUnlocked(const storage::Achievement &achievement) override
+  {
+    Q_UNUSED(achievement);
+    showAchievementUnlockedCallCount++;
+  }
 
-    void showAchievementUnlocked(const storage::Achievement &achievement) override
-    {
-      Q_UNUSED(achievement);
-    }
+  // ИСПРАВЛЕНО: сигнатура синхронизирована с IView (два параметра)
+  void showAchievementsList(const QList< storage::Achievement > &achievements,
+                            const QList< QString > &unlockedAchievementIds) override
+  {
+    Q_UNUSED(achievements);
+    Q_UNUSED(unlockedAchievementIds);
+    showAchievementsListCallCount++;
+  }
 
-    void showAchievementsList(const QList< storage::Achievement >& achievements, const QList< QString >& unlocked_achievement_ids) override
-    {
-      Q_UNUSED(achievements);
-      Q_UNUSED(unlocked_achievement_ids);
-    }
+  void showCampusMap(const QList< QString > &unlockedLocations) override
+  {
+    Q_UNUSED(unlockedLocations);
+    showCampusMapCallCount++;
+  }
 
-    void showCampusMap(const QList<QString> &unlockedLocations) override
-    {
-      Q_UNUSED(unlockedLocations);
-    }
+  void showLocationUnlocked(const QString &locationName) override
+  {
+    Q_UNUSED(locationName);
+    showLocationUnlockedCallCount++;
+  }
 
-    void showLocationUnlocked(const QString &locationName) override
-    {
-      Q_UNUSED(locationName);
-    }
+  void showLevelUpAnimation(int newLevel, const QString &newTitle) override
+  {
+    Q_UNUSED(newLevel);
+    Q_UNUSED(newTitle);
+    showLevelUpAnimationCallCount++;
+  }
 
-    void showLevelUpAnimation(int newLevel, const QString &newTitle) override
-    {
-      Q_UNUSED(newLevel);
-      Q_UNUSED(newTitle);
-    }
+  void updateGamificationPanel() override
+  {
+    updateGamificationPanelCallCount++;
+  }
 
-    void updateGamificationPanel() override
-    {}
+  // ИСПРАВЛЕНО: добавлена реализация нового метода из IView
+  void updateAchievementSlots(const QList< storage::Achievement > &unlockedAchievements) override
+  {
+    Q_UNUSED(unlockedAchievements);
+    updateAchievementSlotsCallCount++;
+  }
 
-    int showTaskListCallCount = 0;
-    int showTasksForDateCallCount = 0;
-    int setTaskListTitleCallCount = 0;
-    int showTaskCreationFormCallCount = 0;
-    int closeTaskCreationFormCallCount = 0;
-    int showErrorMessageCallCount = 0;
-    int showInfoMessageCallCount = 0;
-    int updateStatsCallCount = 0;
+  // Счётчики вызовов (camelCase для совместимости с тестами)
+  int showTaskListCallCount = 0;
+  int showTasksForDateCallCount = 0;
+  int setTaskListTitleCallCount = 0;
+  int showTaskCreationFormCallCount = 0;
+  int closeTaskCreationFormCallCount = 0;
+  int showErrorMessageCallCount = 0;
+  int showInfoMessageCallCount = 0;
+  int updateStatsCallCount = 0;
+  int showUserLevelCallCount = 0;
+  int showStreakCallCount = 0;
+  int showUserTitleCallCount = 0;
+  int showXPNotificationCallCount = 0;
+  int showAchievementUnlockedCallCount = 0;
+  int showAchievementsListCallCount = 0;
+  int showCampusMapCallCount = 0;
+  int showLocationUnlockedCallCount = 0;
+  int showLevelUpAnimationCallCount = 0;
+  int updateGamificationPanelCallCount = 0;
+  int updateAchievementSlotsCallCount = 0;
 
-    QList<storage::Task> lastShownTasks;
-    QDate lastDateArg;
-    QString lastTitle;
-    bool lastFormHadTask = false;
-    storage::Task lastFormTask;
-    QString lastErrorMessage;
-    QString lastInfoMessage;
-    int lastStatsTotal = 0;
-    int lastStatsCompleted = 0;
-    int lastStatsToday = 0;
-  };
+  // Сохранённые данные
+  QList< storage::Task > lastShownTasks;
+  QDate lastDateArg;
+  QString lastTitle;
+  bool lastFormHadTask = false;
+  storage::Task lastFormTask;
+  QString lastErrorMessage;
+  QString lastInfoMessage;
+  int lastStatsTotal = 0;
+  int lastStatsCompleted = 0;
+  int lastStatsToday = 0;
+};
 }
 
 #endif
